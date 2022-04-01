3ae4dab45b6ad5dd7b457ec3154e701fd8c2bde8
Juan <jgarcia1545@gmail.com>
2022-03-31 20:36:53 -0700

    Created the Markdown File Template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if(!license) {
+    return '';
+  }
+
+  return `![License Badge](https://img.shields.io/github/license/${license})
+  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  if(!license) {
+    return '';
+  }
+
+  return `[${license}](https://github.com/${license})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license) {
+    return '';
+  }
+
+  return `
+  ## License
+  Copyright (c) ${renderLicenseLink(license)}. All rights reserved.
+  Licensed under the ${license} license.
+  `;
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Contributing](#contributing)
+  - [Questions](#questions)
+  - [Credits](#credits)
+  - [LIcense](#license)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Tests
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Questions
+  - Here is a link to my [github](https://github.com/${data.github}).
+  - You can email me at ${data.email} with any additional questions.
+
+  ## Credits
+  ${data.credits}
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
